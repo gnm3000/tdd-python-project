4341a678d46ad044a4e7acd3845aba67c5bf0471
German <gnm3000@gmail.com>
2022-01-16 19:13:03 -0300

    first commit

---
+++ ner_client.py
@@ -0,0 +1,24 @@
+class NamedEntityClient:
+    def __init__(self,model,displacy):
+        self.model = model
+        self.displacy = displacy
+        #self.model = spacy.load("en_core_web_sm")
+        
+    def get_ents(self, sentence):
+        doc = self.model(sentence)
+        html = self.displacy.render(doc, style="ent")
+        entities = [{ 'ent': ent.text, 'label': self.map_label(ent.label_) } for ent in doc.ents]
+        return { 'ents': entities, 'html': html }
+    @staticmethod
+    def map_label(label):
+        label_map={
+            "PERSON":"Person",
+            "NORP"  :"Group",
+            "LOC": "Location",
+            "GPE": "Location",
+            
+            "LANGUAGE": "Language",
+            
+            
+        }
+        return label_map.get(label)
